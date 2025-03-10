661cbd1663982893f3e37da13b6fd87942c3f56d
Nelson <nelsonpsouza@gmail.com>
2025-03-09 23:08:21 -0300

    integra viewmodels e uistates a tela de detalhes do produto

--- app/src/main/java/br/com/alura/panucci/navigation/ProductDetailsNavigation.kt
+++ app/src/main/java/br/com/alura/panucci/navigation/ProductDetailsNavigation.kt
@@ -1,12 +1,15 @@
 package br.com.alura.panucci.navigation
 
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
-import br.com.alura.panucci.sampledata.sampleProducts
 import br.com.alura.panucci.ui.screens.ProductDetailsScreen
+import br.com.alura.panucci.ui.viewmodels.ProductDetailsViewModel
 
 private const val PRODUCT_DETAILS_ROUTE = "productDetails"
 private const val PRODUCT_ID_ARGUMENT = "productId"
@@ -15,12 +18,17 @@ fun NavGraphBuilder.productDetailsScreen(navController: NavHostController) {
     composable(
         "$PRODUCT_DETAILS_ROUTE/{$PRODUCT_ID_ARGUMENT}"
     ) { backStackEntry ->
-        val id = backStackEntry.arguments?.getString(PRODUCT_ID_ARGUMENT)
-        sampleProducts.find {
-            it.id == id
-        }?.let { product ->
+
+        val viewModel = viewModel<ProductDetailsViewModel>()
+        val uiState by viewModel.uiState.collectAsState()
+
+        backStackEntry.arguments?.getString(PRODUCT_ID_ARGUMENT)?.let { id ->
+            LaunchedEffect(Unit) {
+                viewModel.findProductById(id)
+            }
+
             ProductDetailsScreen(
-                product = product,
+                uiState = uiState,
                 onNavigateToCheckout = {
                     navController.navigateToCheckout()
                 },
